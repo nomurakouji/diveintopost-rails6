2d7a9d2d4825a67ea04810023aea777855d42981
kouji nomura <nomurakouji0711@yahoo.co.jp>
2022-01-24 23:15:56 +0900

    message

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -15,9 +15,13 @@ class AssignsController < ApplicationController
   end
 
   def destroy
+    # assign（例）
+    # <Assign:0x00007fae403e5210
+    # id: 19,user_id: 5,team_id: 4,
     assign = Assign.find(params[:id])
+    # assign.user
+    # <User id: 5, email: "cory@jerde-lehner.io",icon: nil, keep_team_id: nil>
     destroy_message = assign_destroy(assign, assign.user)
-
     redirect_to team_url(params[:team_id]), notice: destroy_message
   end
 
@@ -27,10 +31,17 @@ class AssignsController < ApplicationController
   end
 
   def assign_destroy(assign, assigned_user)
+    # assign.team.owner
+    # <User id: 1, email: "elenor@schmitt-stokes.name",icon: nil, keep_team_id: 5>
     if assigned_user == assign.team.owner
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
+    # current_user
+    # <User id: 9, email: "christoper_flatley@jast-tillman.com", icon: nil, keep_team_id: 1>
+    elsif ( current_user.id != assign.team.owner.id ) && ( current_user.id != assigned_user.id )
+      set_next_team(assign, assigned_user)
+      I18n.t('views.messages.delete_member')
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       I18n.t('views.messages.delete_member')
@@ -57,12 +68,26 @@ class AssignsController < ApplicationController
     end
   end
 
+  # assigned_user
+  # <User id: 8, email: "solomon.wilkinson@gleichner-torp.io",icon: nil, keep_team_id: nil>
   def set_next_team(assign, assigned_user)
+    # Assign.find_by(user_id: assigned_user.id).team
+    # <Team:0x00007fae3e74fe98
+    # id: 2,(チームの)name: "Zaam-Dox",owner_id: 2,icon: nil>
+
+    # Assign.find_by(user_id: assigned_user.id)
+    # #<Assign:0x00007fae3e35cf98
+    # id: 6,user_id: 8,team_id: 2,
+    # find_by→ 条件に一致するものを引っ張る → user_id: assigned_user.idのとき。
     another_team = Assign.find_by(user_id: assigned_user.id).team
+    # assigned_user.keep_team_id = nil
+    # assign.team_id = 5
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
+    # def change_keep_team(user, current_team)
+    # assigned_user.keep_team_id に assign.team_id を代入
   end
 
-  def find_team(team_id)
+  def find_team(*)
     Team.friendly.find(params[:team_id])
   end
 end
